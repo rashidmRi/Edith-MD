d1bfcf86121bd9bfd373f28434cfdfb681f1d233
rashidmRi <rashidmari302@gmail.com>
2025-11-09 09:46:30 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const staticConfig = {
     // ==================== BAILEYS & SESSION ====================
     BAILEYS: "@whiskeysockets/baileys",
-    SESSION_ID: process.env.SESSION_ID || "EDITH~ujR0",  // Keep session ID in plain text
+    SESSION_ID: process.env.SESSION_ID || "EDITH~kBNn",  // Keep session ID in plain text
     CDN: process.env.CDN || "https://cdn-bandaheali.zone.id",
 };
 
